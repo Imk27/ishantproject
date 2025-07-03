02f15df96a3bbaaba75880df18ea6f4d7417505a
root <root@ip-172-31-1-115.ap-south-1.compute.internal>
2025-07-03 06:39:15 +0000

    Initial commit with app-tier and web-tier

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cd0oss48uxcl.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Ishant1234',
+    DB_DATABASE : 'webappdb'
+});
